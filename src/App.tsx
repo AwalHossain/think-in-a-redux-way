96feb374afba09501548155e5d80eeb9cb342a09
Awal hossain <waqin21@gmail.com>
2022-08-22 19:47:28 +0600

    counter app has been testing with state 💉💉

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css';
+import Counter from './components/Counter';
+import TotalCount from './components/TotalCount';
 
 function App() {
   return (
@@ -7,48 +9,13 @@ function App() {
     <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
         Simple Counter Application
     </h1>
-{/* 
-    <!-- counters --> */}
-    <div className="max-w-md mx-auto mt-10 space-y-5">
-        <div
-            className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
-        >
-            <div className="text-2xl font-semibold" id="counter">4</div>
-            <div className="flex space-x-3">
-                <button
-                    className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-                    id="increment"
-                >
-                    Increment
-                </button>
-                <button
-                    className="bg-red-400 text-white px-3 py-2 rounded shadow"
-                    id="decrement"
-                >
-                    Decrement
-                </button>
-            </div>
-        </div>
-
-        <div
-            className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
-        >
-            <div className="text-2xl font-semibold" id="counter2">0</div>
-            <div className="flex space-x-3">
-                <button
-                    className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-                    id="increment2"
-                >
-                    Increment
-                </button>
-                <button
-                    className="bg-red-400 text-white px-3 py-2 rounded shadow"
-                    id="decrement2"
-                >
-                    Decrement
-                </button>
-            </div>
-        </div>
+    
+    <div>
+        <Counter />
+        <Counter />
+    </div>
+    <div>
+        <TotalCount />
     </div>
 </div>
   );
---
+++ src/components/TotalCount.tsx
@@ -0,0 +1,14 @@
+
+const TotalCount = () => {
+  return (
+    <div className="max-w-md mx-auto mt-10 space-y-5">
+    <div
+        className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
+    >
+        <p className="">Total Count: </p>
+    </div>
+</div>
+  )
+}
+
+export default TotalCount
