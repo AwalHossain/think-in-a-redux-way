9f81c2e859ce30e8df4722ebdef9021012ca52ff
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-04-01 18:36:46 +0000

    todo reducer ✅

---
+++ todo/src/Redux/todos/initialState.js
@@ -0,0 +1,22 @@
+// intial state of the todos reducer
+
+
+
+ const intialState =   [
+            {
+                id: 1,
+                task: 'Learn React',
+                completed: false,
+                color: 'red',
+            },
+            {
+                id: 2,
+                task: 'Learn Redux',
+                completed: false,
+                color: 'red',
+            },
+
+
+
+        ]
+export default intialState;
---
+++ todo/src/Redux/todos/reducer.js
@@ -0,0 +1,68 @@
+// write down action reducers for the todos
+
+import { ADD_TODO, ALLCOMPLETE_TODO, CLEARCOMPLETE_TODO, COLORSELECT_TODO, DELETE_TODO, TOGGLE_TODO } from './actionTypes';
+import initialState from './initialState';
+
+const nextTodoId = (todos)=>{
+    
+    const maxId = todos.reduce((maxId, todo)=>Math.max(todo.id, maxId), -1);
+
+    return maxId;
+    
+}
+
+export const todosReducer = (state = initialState, action) => {
+        switch (action.type) {
+            case ADD_TODO:
+                return[
+                    ...state,
+                    {
+                        id: nextTodoId(state),
+                        task: action.payload,
+                        completed: false
+                    }
+                ]
+            case TOGGLE_TODO:
+                return state.map((todo)=>{
+                    if(todo.id !== action.payload){
+                        return todo;
+                    }
+
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
+                })
+
+            case COLORSELECT_TODO:
+                const {id, color}  = action.payload
+                return state.map((todo)=>{
+                    if(todo.id !== id){
+                        return todo;
+                    }
+                    
+                    return {
+                        ...todo,
+                        color: color,
+                    }
+                })
+
+                case DELETE_TODO:
+                    return state.filter((todo) =>  todo.id !== action.payload)
+                
+                case ALLCOMPLETE_TODO:
+                    return state.map((todo)=>{
+                        return{
+                            ...todo,
+                            completed: true
+                        }
+                    })
+
+
+                case CLEARCOMPLETE_TODO:
+                    return state.filter((todo)=> !todo.completed)
+                    
+            default:
+                return state;
+        }
+}   
