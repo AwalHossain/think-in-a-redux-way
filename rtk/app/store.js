d2ae6c12d130bb83fb06584ffad01f4a345492e7
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-04-14 08:58:13 +0000

    working 🦋🦋

--- rtk/app/store.js
+++ rtk/app/store.js
@@ -1,14 +1,18 @@
-const  { configureStore } = require("@reduxjs/toolkit");
+const  { configureStore, getDefaultMiddleware } = require("@reduxjs/toolkit");
 const dynamicCounterSlice = require("../features/dynamicCounter/dynamicCounterSlice")
+const {createLogger}  = require('redux-logger')
 
-
+const logger = createLogger();
 
 const store = configureStore({
     reducer:{
         dynamicCounter:  dynamicCounterSlice
-    }
+    },
+    middleware: (getDefaultMiddleware)=>
+        getDefaultMiddleware().concat(logger)
+    
 })
 
 
 
-module.exports = store
+module.exports = store;
--- rtk/index.js
+++ rtk/index.js
@@ -1,2 +1,15 @@
 // install react toolkit and redux toolkit  npm install @reduxjs/toolkit react-redux
 
+const { dynamicCounterActions }  = require('./features/dynamicCounter/dynamicCounterSlice')
+
+const store = require('./app/store');
+
+
+store.subscribe(()=>{
+
+})
+
+
+store.dispatch(dynamicCounterActions.increment(3))
+store.dispatch(dynamicCounterActions.increment(3))
+store.dispatch(dynamicCounterActions.increment(1))
