55935a2e47b2aeca407a9e2f88140be548975cb9
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-04-16 09:08:25 +0000

    html template 🏛️

---
+++ video_site/src/components/list/RelatedVideo.tsx
@@ -0,0 +1,12 @@
+import RelatedItemVideo from './RelatedItemList'
+
+export default function RelatedVideo() {
+  return (
+    <div
+      className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto"
+    >
+      <RelatedItemVideo />
+
+    </div>
+  )
+}
