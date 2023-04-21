e66de4a46c74e056aefa548a65c14b0824e7955e
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-04-21 06:00:30 +0000

    handling search 🔎

--- video_site/src/components/navbar/Search.tsx
+++ video_site/src/components/navbar/Search.tsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
+import { useMatch, useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../../app/hooks";
+import { searched } from "../../features/filter/filterSlice";
 
 export default function Search() {
+    const [search, setSearch] = useState<string>("");
+    const dispatch = useAppDispatch();
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        dispatch(searched(search));
+    }
+
+    const match = useMatch("/");
+    const navigate = useNavigate();
+    if (!match) navigate("/");
+
+
     return (
-        <form>
+        <form onSubmit={handleSubmit} >
             <input
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
                 className="outline-none border-none mr-2"
                 type="search"
                 name="search"
