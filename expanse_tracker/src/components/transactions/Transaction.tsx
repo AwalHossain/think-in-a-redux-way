6719ae9f288da699d34afcc6dcc28308eaf0c07a
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-04-26 11:18:51 +0000

    expanse tracker template 🧪🧪

--- expanse_tracker/src/components/transactions/Transaction.tsx
+++ expanse_tracker/src/components/transactions/Transaction.tsx
@@ -1,6 +1,32 @@
+import del from '../../images/delete.svg';
+import edit from '../../images/edit.svg';
+
 
 export default function Transaction() {
     return (
-        <div>Transaction</div>
+        <div>
+            <ul>
+                <li className="transaction income">
+                    <p>Earned this month</p>
+                    <div className="right">
+                        <p>৳ 100</p>
+                        <button className="link">
+                            <img
+                                className="icon"
+                                src={edit}
+                                alt="edit"
+                            />
+                        </button>
+                        <button className="link">
+                            <img
+                                className="icon"
+                                src={del}
+                                alt="delete"
+                            />
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div>
     )
 }
--- expanse_tracker/src/components/transactions/Transactions.tsx
+++ expanse_tracker/src/components/transactions/Transactions.tsx
@@ -1,6 +1,13 @@
+import Transaction from "./Transaction";
 
 export default function Transactions() {
     return (
-        <div>Transactions</div>
+        <div>
+            <p className="second_heading">Your Transactions:</p>
+
+            <div className="conatiner_of_list_of_transactions">
+                <Transaction />
+            </div>
+        </div>
     )
 }
