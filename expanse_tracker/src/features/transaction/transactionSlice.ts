5a26284b3dceb522e683069e045deefff6754743
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-04-27 05:33:52 +0000

    finished slice 🍰

--- expanse_tracker/src/features/transaction/transactionSlice.ts
+++ expanse_tracker/src/features/transaction/transactionSlice.ts
@@ -29,28 +29,32 @@ const initialState: TransactionState = {
 export const fetchTransactions = createAsyncThunk(
   "transaction/fetchTransactions",
   async () => {
-    await getTransactions();
+    const transactions = await getTransactions();
+    return transactions;
   }
 );
 
 export const addTransaction = createAsyncThunk(
   "transaction/addTransaction",
   async (transaction: Transaction) => {
-    await saveTransaction(transaction);
+    const newTransaction = await saveTransaction(transaction);
+    return newTransaction;
   }
 );
 
 export const editTransaction = createAsyncThunk(
   "transaction/editTransaction",
   async (transaction: Transaction) => {
-    await updateTransaction(transaction);
+    const updatedTransaction = await updateTransaction(transaction);
+    return updatedTransaction;
   }
 );
 
 export const deleteTransaction = createAsyncThunk(
   "transaction/deleteTransaction",
   async (id: number) => {
-    await removeTransaction(id);
+    const deletedTransaction = await removeTransaction(id);
+    return deletedTransaction;
   }
 );
 
@@ -58,4 +62,65 @@ const transactionSlice = createSlice({
   name: "transaction",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTransactions.pending, (state, action) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchTransactions.fulfilled, (state, action) => {
+        state.loading = false;
+        state.transactions = action.payload;
+      })
+      .addCase(fetchTransactions.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message ?? null;
+      })
+      .addCase(addTransaction.pending, (state, action) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(addTransaction.fulfilled, (state, action) => {
+        state.loading = false;
+        state.transactions.push(action.payload);
+      })
+      .addCase(addTransaction.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message ?? null;
+      })
+      .addCase(editTransaction.pending, (state, action) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(editTransaction.fulfilled, (state, action) => {
+        state.loading = false;
+        const index = state.transactions.findIndex(
+          (transaction) => transaction.id === action.payload.id
+        );
+        if (index !== -1) {
+          state.transactions[index] = action.payload;
+        }
+      })
+      .addCase(editTransaction.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message ?? null;
+      })
+      .addCase(deleteTransaction.pending, (state, action) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(deleteTransaction.fulfilled, (state, action) => {
+        state.loading = false;
+        state.transactions = state.transactions.filter(
+          (transaction) => transaction.id !== action.payload.id
+        );
+      })
+      .addCase(deleteTransaction.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message ?? null;
+      });
+  },
 });
+
+export default transactionSlice.reducer;
+// export {}
