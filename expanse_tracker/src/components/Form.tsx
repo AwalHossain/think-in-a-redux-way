6f3d7a333c408db660064b8391c8851f1e457a32
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-04-28 03:36:34 +0000

    form data 📅

--- expanse_tracker/src/components/Form.tsx
+++ expanse_tracker/src/components/Form.tsx
@@ -1,53 +1,88 @@
+import React, { useState } from "react";
 
 export default function Form() {
+    const [name, setName] = useState("");
+    const [type, setType] = useState("");
+    const [amount, setAmount] = useState("");
+
+    const reset = () => {
+        setName("");
+        setType("");
+        setAmount("");
+    }
+
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("Hey there");
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000000),
+            name,
+            type,
+            amount: +amount,
+        }
+        console.log(newTransaction);
+
+    }
     return (
         <div>
             <div className="form">
                 <h3>Add new transaction</h3>
-
-                <div className="form-group">
-                    <label htmlFor="transaction_name">Name</label>
-                    <input
-                        type="text"
-                        name="transaction_name"
-                        placeholder="My Salary"
-                    />
-                </div>
-
-                <div className="form-group radio">
-                    <label htmlFor="transaction_type">Type</label>
-                    <div className="radio_group">
+                <form onSubmit={(e) => handleSubmit(e)}>
+                    <div className="form-group">
+                        <label htmlFor="transaction_name">Name</label>
                         <input
-                            type="radio"
-                            value="income"
-                            name="transaction_type"
-                            checked
+                            type="text"
+                            name="transaction_name"
+                            placeholder="My Salary"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+
                         />
-                        <label htmlFor="transaction_type">Income</label>
                     </div>
-                    <div className="radio_group">
+
+                    <div className="form-group radio">
+                        <label htmlFor="transaction_type">Type</label>
+                        <div className="radio_group">
+                            <input
+                                type="radio"
+                                name="transaction_type"
+                                placeholder="Income"
+                                value="income"
+                                checked={type === "income"}
+                                onChange={(e) => setType(e.target.value)}
+
+                            />
+                            <label htmlFor="transaction_type">Income</label>
+                        </div>
+                        <div className="radio_group">
+                            <input
+                                type="radio"
+                                name="transaction_type"
+                                value="expense"
+                                checked={type === "expense"}
+                                onChange={(e) => setType(e.target.value)}
+                            />
+                            <label htmlFor="transaction_type">Expense</label>
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="transaction_amount">Amount</label>
                         <input
-                            type="radio"
-                            value="expense"
-                            name="transaction_type"
-                            placeholder="Expense"
+                            type="number"
+                            placeholder="300"
+                            name="transaction_amount"
+                            value={amount}
+                            onChange={(e) => setAmount(e.target.value)}
                         />
-                        <label htmlFor="transaction_type">Expense</label>
                     </div>
-                </div>
 
-                <div className="form-group">
-                    <label htmlFor="transaction_amount">Amount</label>
-                    <input
-                        type="number"
-                        placeholder="300"
-                        name="transaction_amount"
-                    />
-                </div>
+                    <button className="btn">Add Transaction</button>
 
-                <button className="btn">Add Transaction</button>
+                    <button className="btn cancel_edit">Cancel Edit</button>
+                </form>
 
-                <button className="btn cancel_edit">Cancel Edit</button>
             </div>
         </div>
     )
