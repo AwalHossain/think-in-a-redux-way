c22e35ed6954311200d9cb321c3e06c04c450bb6
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-04-28 12:10:05 +0000

    calculte income 📨

--- expanse_tracker/src/components/Balance.tsx
+++ expanse_tracker/src/components/Balance.tsx
@@ -1,11 +1,25 @@
+import { useAppSelector } from "../app/hooks";
 
 export default function Balance() {
+    const { transactions } = useAppSelector(state => state.transactions);
+    const calculateBalance = () => {
+        let balance = 0;
+        transactions.forEach(transaction => {
+            if (transaction.type === "expense") {
+                balance -= transaction.amount;
+            } else {
+                balance += transaction.amount;
+            }
+        });
+        return balance;
+    }
+
     return (
         <div className="top_card">
             <p>Your Current Balance</p>
             <h3>
                 <span>৳</span>
-                <span>10500</span>
+                <span>{calculateBalance()}</span>
             </h3>
         </div>
     )
